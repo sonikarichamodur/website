5d220990dd41eb2c263be78d79d64aefee846803
Nathan C <nc8774@gmail.com>
2018-11-17 13:39:31 -0500

    Added url map to redirect to /catalog/

--- rlsite/urls.py
+++ rlsite/urls.py
@@ -20,6 +20,11 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
 urlpatterns += [
 	path('catalog/', include('catalog.urls')),
+]
+urlpatterns += [
+	path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
