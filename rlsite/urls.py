171606f658ca982829b90b839f03a508edf5fac2
Nathan C <nc8774@gmail.com>
2018-12-01 14:09:05 -0500

    Build worked, but forgot to remove catalog

--- rlsite/urls.py
+++ rlsite/urls.py
@@ -19,5 +19,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('catalog/', include('catalog.urls')),
 	path('', RedirectView.as_view(url='/catalog/', permanent=True)),]
