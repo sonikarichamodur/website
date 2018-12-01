c9f05a6613fca5fe9164e3bf7c2387f968fa9885
Nathan C <nc8774@gmail.com>
2018-12-01 14:42:45 -0500

    Made the base URL load the blog

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='blog:home')),
     path('register/', RegisterView.as_view(), name='register'),
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('/', include('blog.urls')),
 ]
