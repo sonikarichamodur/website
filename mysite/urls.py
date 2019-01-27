b3a20d46c007ddabca1f88686b39cba7c2501628
Nathan C <nc8774@gmail.com>
2019-01-27 16:02:43 -0500

    Finished making navbar changeable

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from blog.views.home import about
+from blog.views.home import nav
 
 from blog.views.register import RegisterView
 
 urlpatterns = [
-    path('about', about, name='about'),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
@@ -29,4 +28,5 @@ urlpatterns = [
 #   path('register/', RegisterView.as_view(), name='register'),
     path('', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
+    path('<str:link>', nav, name='nav'),
 ]
