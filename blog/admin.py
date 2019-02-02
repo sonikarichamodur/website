efb3a9bde8e07a906b3e6dae97fa172f4ec9968b
Nathan C <nc8774@gmail.com>
2019-02-02 16:55:56 -0500

    Added main text to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,10 @@ from .models.comment import Comment
 from .models.post import Post
 from .models.nav import Nav
 from .models.files import Files
+from .models.maintext import MainText
 
 admin.site.register(Nav)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Files)
+admin.site.register(MainText)
