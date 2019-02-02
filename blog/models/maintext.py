0184f747a4428a4314902328092c7d27ac81e615
Nathan C <nc8774@gmail.com>
2019-02-02 16:58:36 -0500

    Got rid of uneccesary user and title data

--- blog/models/maintext.py
+++ blog/models/maintext.py
@@ -4,8 +4,5 @@ from django.urls import reverse
 
 
 class MainText(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
-    link = models.SlugField(max_length=10)
     body = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
