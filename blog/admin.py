7902b9eff1d55b49e4d5c6ff6939b58b177d7030
Paulson McIntyre <paul@gpmidi.net>
2019-02-17 14:21:38 -0500

    Fixed admin site

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,29 @@ from .models.post import Post
 from .models.nav import Nav
 from .models.files import Files
 from .models.maintext import MainText
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models.users import Details
+
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class DetailsInline(admin.StackedInline):
+    model = Details
+    can_delete = False
+    verbose_name_plural = 'details'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (DetailsInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Nav)
 admin.site.register(Post)
