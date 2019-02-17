7f0d3136ea24facdea4165b9fc1a776f312de0ce
Paulson McIntyre <paul@gpmidi.net>
2019-02-17 14:19:37 -0500

    Fixed up a few things and changed display name to use display_name

--- blog/models/users.py
+++ blog/models/users.py
@@ -5,3 +5,10 @@ from django.db import models
 class Details(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=100, verbose_name="Display Name")
+
+    def display(self):
+        if self.display_name:
+            return self.display_name
+        if self.user.first_name and self.user.last_name:
+            return self.user.first_name + " " + self.user.last_name
+        return self.user.username
