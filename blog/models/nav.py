e4ac496703d86163d8a22c07093916d1024c10a8
Paulson McIntyre <paul@gpmidi.net>
2019-02-17 19:44:30 -0500

    Adding parent field to nav

--- blog/models/nav.py
+++ blog/models/nav.py
@@ -9,10 +9,11 @@ class Nav(models.Model):
     link = models.SlugField(max_length=10)
     body = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+    parent = models.ForeignKey('Nav', null=True, default=None, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('blog:post', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return '"{title}" by {username}'.format(title=self.title,
-                                                username=self.user.username)
+        return '"{title}" by {name}'.format(title=self.title,
+                                            name=self.user.details.display() or self.user.first_name)
