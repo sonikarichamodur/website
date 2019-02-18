dd3ddf44437cd91bf477f23d58ae57058ca120cd
Paulson McIntyre <paul@gpmidi.net>
2019-02-17 21:13:32 -0500

    Formatting

--- blog/ctx.py
+++ blog/ctx.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from blog.models.nav import Nav
 
+
 def heroku_info(request):
     return {
         'HEROKU_RELEASE_VERSION': settings.HEROKU_RELEASE_VERSION,
         'HEROKU_APP_NAME': settings.HEROKU_APP_NAME,
         'HEROKU_SLUG_COMMIT': settings.HEROKU_SLUG_COMMIT,
     }
+
 
 def nav(request):
     return {
