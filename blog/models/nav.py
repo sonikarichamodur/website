3b21ea5d5f6b1148403d7e5bac323613f464051a
Paulson McIntyre <paul@gpmidi.net>
2019-02-17 21:07:57 -0500

    Making a few nav fields unique and trying to fix parent

--- blog/models/nav.py
+++ blog/models/nav.py
@@ -6,11 +6,11 @@ from blog.models.users import Details
 
 class Nav(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
-    link = models.SlugField(max_length=10)
+    title = models.CharField(max_length=20, unique=True)
+    link = models.SlugField(max_length=10, unique=True)
     body = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
-    parent = models.ForeignKey('self', null=True, default=None, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse('blog:post', kwargs={'pk': self.pk})
