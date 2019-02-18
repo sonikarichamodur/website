4ad1b96b6c82439de7787d77342a602ec441b13d
Paulson McIntyre <paul@gpmidi.net>
2019-02-17 21:42:54 -0500

    Adding text type field for texts

--- blog/models/maintext.py
+++ blog/models/maintext.py
@@ -4,5 +4,10 @@ from django.urls import reverse
 
 
 class MainText(models.Model):
+    TEXT_TYPE_CHOICES = (
+        ('Draft', 'Draft'),
+        ('Main Page', 'Main'),
+    )
     body = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+    text_type = models.CharField(max_length=254, default='Draft', choices=TEXT_TYPE_CHOICES)
