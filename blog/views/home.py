1200830f3e3aa770aa69595e3398a9ea3b7fd73e
Paulson McIntyre <paul@gpmidi.net>
2019-02-18 06:40:25 -0500

    Adding 404 on no user

--- blog/views/home.py
+++ blog/views/home.py
@@ -15,7 +15,10 @@ def home(request, username=None):
     user = None
     main_text = MainText.objects.filter(text_type='Main Page').order_by('pk')[0]
     if username:
-        user = User.objects.get(username=username)
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise Http404()
         first_name = user.first_name
         last_name = user.last_name
         post_list = Post.objects.filter(user=user)
