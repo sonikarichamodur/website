6855193b1c241c627fa5e3ad33e3c55c1b72059d
Paulson McIntyre <paul@gpmidi.net>
2019-03-15 05:44:35 -0400

    Model progress for scout

--- scout/models.py
+++ scout/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.postgres.fields import HStoreField
+from django.contrib.postgres.fields import HStoreField, ArrayField
 from django.core.validators import *
 
 
@@ -50,3 +50,68 @@ class TeamRobot(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     # Name of the robot as provided by the team.
     robot_name = models.CharField(max_length=512)
+
+
+class Event(models.Model):
+    key = models.CharField(primary_key=True, max_length=512)
+    name = models.CharField(max_length=1024)
+    event_code = models.CharField(max_length=255)
+    # event_type = models.IntegerField() #FIXME: Lookup values https://github.com/the-blue-alliance/the-blue-alliance/blob/master/consts/event_type.py#L2
+    # # district
+    # city
+    # state_prov
+    # country
+    # start_date
+    # end_date
+    # year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    # short_name
+    # event_type_string
+    # week
+    # address
+    # postal_code
+    # gmaps_place_id
+    # gmaps_url
+    # lat
+    # lng
+    # location_name
+    # timezone
+    # website
+    # first_event_id
+    # first_event_code
+    # #webcasts
+    # division_keys
+    # parent_event_key
+    # playoff_type
+    # playoff_type_string
+
+
+class Award(models.Model):
+    name = models.CharField(max_length=1024)
+    award_type = models.CharField(max_length=1024)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+
+
+class AwardRecipient(models.Model):
+    award = models.ForeignKey(Award, on_delete=models.CASCADE, related_name='recipient_list')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    awardee = models.CharField(max_length=1024)
+
+
+class DistrictRanking(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    rank = models.IntegerField()
+    rookie_bonus = models.IntegerField()
+    point_total = models.IntegerField()
+    # event_points
+
+
+class DistrictRankingEventPoints(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    district_ranking = models.ForeignKey(DistrictRanking, on_delete=models.CASCADE)
+    district_cmp = models.BooleanField()
+    alliance_points = models.IntegerField()
+    award_points = models.IntegerField()
+    qual_points = models.IntegerField()
+    elim_points = models.IntegerField()
+    total = models.IntegerField()
