e6399ef5532c54b4b650ad8dba7373a6d1ba9710
Paulson McIntyre <paul@gpmidi.net>
2019-09-21 16:27:33 -0400

    Auto format

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.shortcuts import redirect
 
-
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
     path('nested_admin/', include('nested_admin.urls')),
