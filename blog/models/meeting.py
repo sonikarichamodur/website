905c1cb923ed93743b97cfb70c321798140bc19c
AyushHariharan <ayush0624@gmail.com>
2019-09-26 18:18:15 -0400

    added default endtime instead of start time

--- blog/models/meeting.py
+++ blog/models/meeting.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
+from djanbo.utils import timezone
 from blog.models.users import Details
 from blog.models.post import Post
 
 
 class Meeting(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    start_time = models.DateTimeField('meeting start time', default=timezone.now, auto_now_add=True, editable=True)
-    end_time = models.DateTimeField('meeting end time', null=True)
+    start_time = models.DateTimeField('meeting start time', auto_now_add=True, editable=True)
+    end_time = models.DateTimeField('meeting end time', default=timezone.now, null=True)
 
     def __str__(self):
         return "meeting from {start_time} to {end_time}".format(start_time=self.start_time, end_time=self.end_time)
