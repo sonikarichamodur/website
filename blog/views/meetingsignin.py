6fc05dfd7f048028e2abb61cd3aa9ac04ca32837
AyushHariharan <ayush0624@gmail.com>
2019-10-05 15:29:10 -0400

    changed self to form

--- blog/views/meetingsignin.py
+++ blog/views/meetingsignin.py
@@ -30,7 +30,7 @@ class MeetingSignin(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
         if form.instance.meeting.end_time < timezone.now():
             return HttpResponse('meeting has ended', status=500)
 
-        if Signin.objects.filter(meeting=form.instance.meeting, user__id=self.kwargs['user'],
+        if Signin.objects.filter(meeting=form.instance.meeting, user__id=form.kwargs['user'],
                                  end_time__isnull=True).count() > 0:
             return redirect("/meeting/%d/" % form.instance.meeting.id)
 
