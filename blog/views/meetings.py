3cc0742ce0d80350d0aba85673f5be68a1933c9e
AyushHariharan <ayush0624@gmail.com>
2019-10-05 15:12:54 -0400

    updating endtime after meeting creation

--- blog/views/meetings.py
+++ blog/views/meetings.py
@@ -9,13 +9,14 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 
 class MeetingCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Meeting
-    fields = ['end_time', ]
+    fields = ['end_time']
     template_name = 'blog/create_meeting.html'
     login_url = reverse_lazy('login')
     permission_required = "blog.meeting_gui_can_create"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        
         return super().form_valid(form)
 
     def get_success_url(self):
