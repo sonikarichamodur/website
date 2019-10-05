79719980bcbf975849ae7189996801152bf6d7cd
AyushHariharan <ayush0624@gmail.com>
2019-10-05 16:19:30 -0400

    typo again

--- blog/views/meetingsignin.py
+++ blog/views/meetingsignin.py
@@ -48,7 +48,7 @@ class MeetingSignin(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     def get_context_data(self, **kwargs):
         ctx = super(MeetingSignin, self).get_context_data(**kwargs)
         signins = Signin.objects.filter(meeting=Meeting.objects.get(pk=self.kwargs['pk']), end_time__isnull=True).all()
-        usr = Member.objects.filter(Q(signin_set__contains=signins) | Q(signin_set__count=0))
+        usr = Member.objects.filter(Q(signin__contains=signins) | Q(signin__count=0))
         ctx['form'].fields['user'].queryset = usr
         ctx['signed_in'] = Signin.objects.filter(end_time__isnull=True,
                                                  meeting=Meeting.objects.get(pk=self.kwargs['pk'])).all()
