2ddfd36cb31d649b4ebb119aa1edc3045f040927
Paulson McIntyre <paul@gpmidi.net>
2019-10-10 16:52:27 -0400

    Making slack id mandatory again

--- blog/models/member.py
+++ blog/models/member.py
@@ -7,7 +7,7 @@ from blog.models.post import Post
 class Member(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField('member name', max_length=4096)
-    slack = models.CharField('slack id', max_length=4096, blank=True, null=True)
+    slack = models.CharField('slack id', max_length=4096)
     created = models.DateTimeField('user added', auto_now_add=True)
     modified = models.DateTimeField('user modified', auto_now=True)
 
