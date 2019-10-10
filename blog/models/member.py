996dec2d868346de5dcacc9fd69720565e1ccc3d
Paulson McIntyre <paul@gpmidi.net>
2019-10-10 16:52:11 -0400

    Making member a bit more tolerant of weirdness

--- blog/models/member.py
+++ blog/models/member.py
@@ -5,9 +5,9 @@ from blog.models.post import Post
 
 
 class Member(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField('member name', max_length=4096)
-    slack = models.CharField('slack id', max_length=4096)
+    slack = models.CharField('slack id', max_length=4096, blank=True, null=True)
     created = models.DateTimeField('user added', auto_now_add=True)
     modified = models.DateTimeField('user modified', auto_now=True)
 
