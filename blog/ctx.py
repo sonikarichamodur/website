d129922ef1667d977a70d395322a49a17d928895
Paulson McIntyre <paul@gpmidi.net>
2019-10-10 17:45:38 -0400

    Testing with signins

--- blog/ctx.py
+++ blog/ctx.py
@@ -3,7 +3,7 @@ from .models.nav import Nav
 from .models.maintext import MainText
 from .models.meeting import Meeting
 from django.utils import timezone
-from django.db.models import Q
+from django.db.models import Q, Count
 
 
 def heroku_info(request):
@@ -19,7 +19,7 @@ def nav(request):
         'nav_items': Nav.objects.filter(parent=None).order_by("title").all(),
         'meetings': Meeting.objects.filter(start_time__lte=timezone.now()).filter(
             Q(end_time__isnull=True) | Q(end_time__gte=timezone.now())
-        ).all(),
+        ).annotate(signins=Count('signins')).all(),
     }
 
 
