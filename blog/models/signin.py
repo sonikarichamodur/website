77c805ccad86b1232622df6dbc8cf4dc4bc812d6
Paulson McIntyre <paul@gpmidi.net>
2019-10-19 17:05:33 -0400

    Switching to vol validators

--- blog/models/signin.py
+++ blog/models/signin.py
@@ -24,29 +24,44 @@ def endValidator(signin):
 
 
 class Signin(models.Model):
+    def meetingValidator(signin):
+        if signin.meeting is not None:
+            return Signin.objects.filter(
+                user=signin.user,
+                meeting=signin.meeting,
+                start_time__isnull=False,
+                end_time__isnull=True,
+            ).count() == 0
+
+        return True
+
     user = models.ForeignKey(Member, on_delete=models.CASCADE, null=False)
-    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, null=False)
-    start_time = models.DateTimeField('sign-in time', auto_now_add=True, null=False)
-    end_time = models.DateTimeField('sign-out time', null=True)
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, null=False, validators=[meetingValidator, ])
+    start_time = models.DateTimeField('sign-in time', auto_now_add=True, null=False, validators=[startValidator, ])
+    end_time = models.DateTimeField('sign-out time', null=True, validators=[endValidator, ]),
 
-    def clean(self):
-        # Skip this if self.meeting isn't defined yet
-        try:
-            if Signin.objects.filter(
-                    user=self.user,
-                    meeting=self.meeting,
-                    start_time__isnull=False,
-                    end_time__isnull=True,
-            ).count() > 0:
-                raise ValidationError("User already signed in")
-        except Signin.meeting.RelatedObjectDoesNotExist as e:
-            pass
-
-        if not startValidator(self):
-            raise ValidationError("Invalid start time")
-
-        if not endValidator(self):
-            raise ValidationError("User didn't sign out")
+    #
+    # def clean(self):
+    #     # Not really needed
+    #     super().clean()
+    #
+    #     # Skip this if self.meeting isn't defined yet
+    #     try:
+    #         if Signin.objects.filter(
+    #                 user=self.user,
+    #                 meeting=self.meeting,
+    #                 start_time__isnull=False,
+    #                 end_time__isnull=True,
+    #         ).count() > 0:
+    #             raise ValidationError("User already signed in")
+    #     except Signin.meeting.RelatedObjectDoesNotExist as e:
+    #         pass
+    #
+    #     if not startValidator(self):
+    #         raise ValidationError("Invalid start time")
+    #
+    #     if not endValidator(self):
+    #         raise ValidationError("User didn't sign out")
 
     def __str__(self):
         return "{user} signed in to {meeting} from {start_time} to {end_time}".format(user=self.user.name,
