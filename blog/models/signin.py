4bd3efd42aa7d06ac683dfb41232c0635fa11f62
Paulson McIntyre <paul@gpmidi.net>
2019-10-19 18:10:37 -0400

    log++

--- blog/models/signin.py
+++ blog/models/signin.py
@@ -2,21 +2,27 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
-
+import logging
 from blog.models.users import Details
 from blog.models.post import Post
 from blog.models.member import Member
 from blog.models.meeting import Meeting
 
+base_log = logging.getLogger('blog.models.signin')
+
 
 class Signin(models.Model):
     def meetingValidator(signin):
-        return signin.meeting and signin.user and Signin.objects.filter(
+        log = base_log.getChild("meetingValidator")
+        log.debug("In meeting validator", extra=dict(signin=signin))
+        ret = signin.meeting and signin.user and Signin.objects.filter(
             user=signin.user,
             meeting=signin.meeting,
             start_time__isnull=False,
             end_time__isnull=True,
         ).count() == 0
+        log.debug("End meeting validator", extra=dict(signin=signin, ret=ret))
+        return ret
 
     def startValidator(signin):
         if signin.start_time is not None:
