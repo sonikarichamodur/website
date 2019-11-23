dfe9af018c5a284d87170590321bf344c806876f
Shravan Athikinasetti <shravan.athikinasetti@gmail.com>
2019-11-23 15:24:17 -0500

    Added team column to members

--- blog/models/member.py
+++ blog/models/member.py
@@ -6,9 +6,20 @@ from simple_history.models import HistoricalRecords
 
 
 class Member(models.Model):
+    TEAM = (
+        ("None", "None"),
+        ("Programming/Electrical","Programming/Electrical"),
+        ("CAD/Manufacturing","CAD/Manufacturing"),
+        ("Outreach","Outreach"),
+        ("Strategy","Strategy"),
+        ("Safety","Safety"),
+        ("Helping Hands","Helping Hands")
+        
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField('member name', max_length=4096)
     slack = models.CharField('slack id', max_length=4096, blank=True, null=True)
+    team = models.CharField('team name', max_length=255, blank=False, null=False ,choices=TEAM, default="None")
     created = models.DateTimeField('user added', auto_now_add=True)
     modified = models.DateTimeField('user modified', auto_now=True)
     history = HistoricalRecords()
