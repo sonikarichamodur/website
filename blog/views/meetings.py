d4740e14b8e204c1bd72649815a05d788ad0e7be
AyushHariharan <ayush0624@gmail.com>
2019-12-12 18:44:47 -0500

    fixed meeting instance

--- blog/views/meetings.py
+++ blog/views/meetings.py
@@ -29,36 +29,8 @@ def meeting(request):
         m = Meeting(user=request.user)
         m.save()
         for choice in form.cleaned_data['choices']:
-            ch = MeetingType(meeting=m.id, subteam=choice)
+            ch = MeetingType(meeting=m, subteam=choice)
             ch.save()
             return HttpResponseRedirect(reverse('blog:signin', kwargs={'pk': m.pk}))
 
     return render(request, 'blog/create_meeting.html', {"form": form})
-
-
-class MeetingCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    model = Meeting
-    fields = []
-    template_name = 'blog/create_meeting.html'
-    login_url = reverse_lazy('login')
-    permission_required = "blog.meeting_gui_can_create"
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-
-        # if self.kwargs['end_time'] <= (timezone.now() + timedelta(minutes=25)):
-        #     return HttpResponse("meeting end time is set incorrectly", status=500)
-
-        ret = super().form_valid(form)
-        #
-        # if self.object.end_time <= (timezone.now() + timedelta(minutes=25)):
-        #     return HttpResponse("meeting end time is set incorrectly", status=500)
-
-        # if Meeting.objects.filter(start_time__lte=timezone.now()).filter(
-        #         Q(end_time__isnull=True) | Q(end_time__gte=timezone.now())).count() > 0:
-        #     raise forms.ValidationError("cannot create multiple meetings")
-
-        return ret
-
-    def get_success_url(self):
-        return reverse('blog:signin', kwargs={'pk': self.object.pk})
